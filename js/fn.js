d52f5aae6873f7a2d9ba00a0bcb9254d734818e3
Doonngg <61999334+Doonngg@users.noreply.github.com>
2022-04-27 14:36:56 +0800

    Add files via upload

---
+++ js/fn.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+var foldBtn = document.querySelector(".fold-btn");
+var closeBtn = document.querySelector(".close-btn");
+
+foldBtn.addEventListener("click",function(){
+	myAnimate(".menu-list","100%" ,"height","height .3s");
+})
+closeBtn.addEventListener("click",function(){
+	myAnimate(".menu-list","0" ,"height","height .3s");
+})
+
+
+
+
+function myAnimate(animateNode, animateEnd, animateStyle, animateTransition) {
+			document.querySelector(animateNode).style.transition = animateTransition;
+			document.querySelector(animateNode).style[animateStyle] = animateEnd;
+		}
